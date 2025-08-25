8a79d49d648590f95fd6a6981c6eeaf46e6e4d56
hackiado <seidogitan@gmail.com>
2025-08-25 19:47:56 +0200

    clean code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
@@ -12,91 +13,88 @@
 
 using namespace Dualys;
 using namespace std::chrono_literals;
+
 constexpr int BASE_PORT = 8000;
-constexpr int kListenBacklog = 3;
+constexpr int kListenBacklog = 8;
 inline constexpr auto kServerStartupDelay = 100ms;
 constexpr int kNodeCount = 8;
 
-inline void wait_for_server_startup() noexcept
-{
+inline void wait_for_server_startup() noexcept {
     std::this_thread::sleep_for(kServerStartupDelay);
 }
 
-// Fonction qui sera exécutée par chaque nœud (processus enfant)
-void run_node_logic(const Node& node) {
-    std::cout << "[Node " << node.id << "] Process " << getpid() << " is alive. Listening on port " << BASE_PORT + node.id << std::endl;
+void send_value(const int sock, const uint32_t value) {
+    const uint32_t network_value = htonl(value);
+    send(sock, &network_value, sizeof(network_value), 0);
+}
 
-    // --- 1. Création du socket d'écoute (partie "serveur") ---
+unsigned int receive_value(const int sock) {
+    int network_value = 0;
+    read(sock, &network_value, sizeof(network_value));
+    return ntohl(network_value);
+}
+
+void run_node_logic(const Node &node) {
     const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("[Node Error] socket failed");
-        exit(EXIT_FAILURE);
-    }
-
     sockaddr_in address{};
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(BASE_PORT + node.id);
-
-    // Attacher le socket au port
-    if (bind(server_fd, reinterpret_cast<sockaddr *>(&address), sizeof(address)) < 0) {
-        perror("[Node Error] bind failed");
+    if (bind(server_fd, reinterpret_cast<sockaddr *>(&address), sizeof(address)) < 0 || listen(
+            server_fd, kListenBacklog) < 0) {
+        perror("[Node Error] Server setup failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, kListenBacklog) < 0) {
-        perror("[Node Error] listen failed");
-        exit(EXIT_FAILURE);
-    }
-    std::cout << "[Node " << node.id << "] Is now listening for connections..." << std::endl;
 
     wait_for_server_startup();
 
-    for (const int neighbor_id : node.neighbors) {
-        if (neighbor_id > node.id) {
-            std::cout << "[Node " << node.id << "] Attempting to connect to neighbor " << neighbor_id << "..." << std::endl;
+    unsigned int local_sum = node.id;
+    std::cout << "[Node " << node.id << "] Starting with value: " << local_sum << std::endl;
+
+    for (int d = 0; d < 3; ++d) {
+        const auto partner_id = node.id ^ 1 << d;
+
+        if (node.id > partner_id) {
             const int client_sock = socket(AF_INET, SOCK_STREAM, 0);
             sockaddr_in serv_addr{};
             serv_addr.sin_family = AF_INET;
-            serv_addr.sin_port = htons(BASE_PORT + neighbor_id);
+            serv_addr.sin_port = htons(BASE_PORT + partner_id);
             inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-
-            if (connect(client_sock, reinterpret_cast<sockaddr *>(&serv_addr), sizeof(serv_addr)) < 0) {
-                std::cerr << "[Node Error] Connection to neighbor " << neighbor_id << " failed." << std::endl;
-            } else {
-                std::cout << "[Node " << node.id << "] Successfully connected to neighbor " << neighbor_id << "!" << std::endl;
-                std::string msg = "Hello from Node " + std::to_string(node.id);
-                send(client_sock, msg.c_str(), msg.length(), 0);
+            if (connect(client_sock, reinterpret_cast<sockaddr *>(&serv_addr), sizeof(serv_addr)) == 0) {
+                std::cout << "[Node " << node.id << "] Round " << d << ": Sending value " << local_sum << " to partner "
+                        << partner_id << std::endl;
+                send_value(client_sock, local_sum);
                 close(client_sock);
+            } else {
+                perror("[Node Error] Connection failed");
             }
+            break;
+        }
+        int new_socket;
+        int addrlen = sizeof(address);
+        if ((new_socket = accept(server_fd, reinterpret_cast<sockaddr *>(&address),
+                                 reinterpret_cast<socklen_t *>(&addrlen))) >= 0) {
+            const unsigned int received_value = receive_value(new_socket);
+            local_sum += received_value;
+            std::cout << "[Node " << node.id << "] Round " << d << ": Received value " << received_value <<
+                    " from partner " << partner_id << ". New sum: " << local_sum << std::endl;
+            close(new_socket);
+        } else {
+            perror("accept");
         }
     }
-
-    std::cout << "[Node " << node.id << "] Waiting to accept connections..." << std::endl;
-    for (const int neighbor_id: node.neighbors) {
-        if (neighbor_id < node.id) {
-            int new_socket;
-            int addrlen = sizeof(address);
-            if ((new_socket = accept(server_fd, reinterpret_cast<sockaddr *>(&address),
-                                     reinterpret_cast<socklen_t *>(&addrlen))) < 0) {
-                perror("accept");
-            } else {
-                char buffer[1024] = {};
-                read(new_socket, buffer, 1024);
-                std::cout << "[Node " << node.id << "] Received message: '" << buffer << "' from neighbor " <<
-                        neighbor_id << std::endl;
-                close(new_socket);
-            }
-        }
+    if (node.id == 0) {
+        std::cout << "------------------------------------------" << std::endl;
+        std::cout << "[Node 0] FINAL RESULT: The total sum is " << local_sum << std::endl;
+        std::cout << "------------------------------------------" << std::endl;
     }
     close(server_fd);
-    std::cout << "[Node " << node.id << "] Finished." << std::endl;
 }
 
 static std::vector<pid_t> launch_nodes(const HamonCube &cube) {
     std::vector<pid_t> childPids;
     childPids.reserve(kNodeCount);
-
     for (int i = 0; i < kNodeCount; ++i) {
         const pid_t pid = fork();
         if (pid == 0) {
@@ -122,7 +120,6 @@ static void wait_for_children(const std::vector<pid_t> &pids) {
 
 int main() {
     std::cout << "Orchestrator starting... (PID: " << getpid() << ")" << std::endl;
-
     const HamonCube cube;
     const auto childPids = launch_nodes(cube);
 
