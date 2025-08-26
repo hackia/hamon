6615335ea0c84b0b478152083a64d372b8ec7819
hackiado <seidogitan@gmail.com>
2025-08-26 14:43:31 +0200

    remove unseless files

--- _deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seido/CLionProjects/hamon/_deps/googletest-src
-# Build directory: /home/seido/CLionProjects/hamon/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
