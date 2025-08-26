67594f6c671faf990815982f8c69a49ec964b8cd
hackiado <seidogitan@gmail.com>
2025-08-26 14:46:17 +0200

    clean test

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seido/CLionProjects/hamon
-# Build directory: /home/seido/CLionProjects/hamon
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/seido/CLionProjects/hamon/hamon_tests[1]_include.cmake")
